36a8a214069abf59177be812a2347500fd82e690
Gabriel da Silva Alves <gabriel.gallhardo@gmail.com>
2018-08-28 08:07:44 -0300

    Initial Commit

---
+++ grafosMack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  grafosMack
+//
+//  Created by Gabriel da Silva Alves on 28/08/18.
+//  Copyright © 2018 Gabriel da Silva Alves. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
